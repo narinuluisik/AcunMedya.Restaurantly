561962bb1b8b371addca789025466ab6f7abece3
narin uluışık <narinuluisik7@gmail.com>
2025-01-30 00:27:48 +0300

    Proje dosyası ekle.

---
+++ AcunMedya.Restaurantly/Controllers/DefaultController.cs
@@ -0,0 +1,74 @@
+﻿using AcunMedya.Restaurantly.Context;
+using AcunMedya.Restaurantly.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunMedya.Restaurantly.Controllers
+{
+    public class DefaultController : Controller
+    {
+        RestaurantlyContext Db = new RestaurantlyContext();
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialTop()
+        {
+            ViewBag.Call = Db.Adresss.Select(x => x.Call).FirstOrDefault();
+            ViewBag.OpenHours= Db.Adresss.Select(x => x.OpenHours).FirstOrDefault();
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFeature()
+        {
+            ViewBag.SubTitle = Db.Features.Select(x => x.SubTitle).FirstOrDefault();
+            ViewBag.Title = Db.Features.Select(x => x.Title).FirstOrDefault();
+            ViewBag.VideoUrl = Db.Features.Select(x => x.VideoUrl).FirstOrDefault();
+           // ViewBag.OpenHours = Db.Adresss.Select(x => x.OpenHours).FirstOrDefault();
+            return PartialView();
+        }
+        public PartialViewResult PartialAbout()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialService()
+        {
+            var values = Db.Services.ToList();
+            return PartialView(values);
+        }
+
+    
+        public PartialViewResult PartialMenu()
+        {
+            var value = Db.Products.ToList();
+            return PartialView(value);
+        }
+
+        public PartialViewResult PartialContact()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult ContactAdd(Contact model)
+        {
+            model.SendDate = DateTime.Now;
+            model.IsRead = false;
+            Db.Contacts.Add(model);
+            Db.SaveChanges();
+            ViewBag.Message = "Mesaj Başarılı";
+            return View("Index");
+
+        }
+    }
+}
